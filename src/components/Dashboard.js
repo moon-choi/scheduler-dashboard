f1af9985103f7f4e3ee7d965d8fbb3980dc0324a
Moon Choi <choimy1991@gmail.com>
2022-05-26 19:09:34 -0700

    Handling State

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,21 +2,73 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
+
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
 
 class Dashboard extends Component {
-
+  //initial state
   state = {
-    loading: true
+    loading: false,
+    focused: null
   };
 
+  //state setter
+  selectPanel(id) {
+    this.setState({
+      focused: id
+    });
+  }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    return <main className={dashboardClasses} />;
+    // We filter the data first. The condition that we filter panels on depends on the this.state.focused value. If this.state.focused is null then return true for every panel. If this.state.focused is equal to the Panel, then let it through the filter.
+
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={this.selectPanel}
+        />
+      ));
+
+    return (
+      <main className={dashboardClasses}>
+        {panels}
+      </main>
+    );
   }
 
 }
@@ -24,8 +76,16 @@ class Dashboard extends Component {
 
 // different syntax
 
-/*function Dashboard(props) {
+/*
+function Dashboard(props) {
+  const [state, setState] = React.useState({focused: null});
   const dashboardClasses = classnames("dashboard");
+
+  function selectPanel(id) {
+    setState({
+      focused: id
+    });
+  }
   return <main className={dashboardClasses} />;
 }
 */
