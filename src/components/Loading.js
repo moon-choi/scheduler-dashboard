cb596e0c00cead3bd180bf3e213264d2817b2a0f
Moon Choi <choimy1991@gmail.com>
2022-05-26 18:09:21 -0700

    Loading Component

---
+++ src/components/Loading.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+export default class Loading extends Component {
+  state = { loading: true }
+  render() {
+    return <section className="loading">Loading</section>;
+  }
+}
